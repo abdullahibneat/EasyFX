0fa5078dc1eeca72bf51b84cb623e2cd71923928
Abdullah Ibne Atiq <iAbdullah@live.it>
2018-12-30 18:25:38 +0100

    Moved features section to style.css, move js to custom.js, add instagram feed, fix css

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+function easy_nav_toggle() {
+  const navs = document.querySelectorAll('.easy_nav')
+  navs.forEach(nav => nav.classList.toggle('easy_nav-toggle-show'));
+  document.getElementById('easy_nav-container').classList.toggle('easy_nav-mobile');
+  document.getElementsByClassName('easy_nav-toggle')[0].classList.toggle('easy_nav-toggle-show')
+  document.getElementById('easy_logo').classList.toggle('easy_logo-mobile')
+}
+
+function navScroll() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById('easy_nav-container').classList.add('easy_nav-scroll')
+    document.getElementById('easy_logo').classList.add('easy_logo-scroll')
+  } else {
+    document.getElementById('easy_nav-container').classList.remove('easy_nav-scroll')
+    document.getElementById('easy_logo').classList.remove('easy_logo-scroll')
+  }
+}
+
+AOS.init({
+  duration: 1000,
+  once: true,
+});
+
+document.querySelector('.easy_nav-toggle').addEventListener('click', easy_nav_toggle);
+window.onscroll = function() {navScroll()};
