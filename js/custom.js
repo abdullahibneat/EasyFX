446cf3f2ff8caa7cad3ade1ef3be8f670583ba33
Abdullah Ibne Atiq <abdullahibneat@users.noreply.github.com>
2019-01-11 09:55:01 +0000

    Update fontawesome, finalize footer, add favicon

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,20 @@
 function easy_nav_toggle() {
-  const navs = document.querySelectorAll('.easy_nav')
-  navs.forEach(nav => nav.classList.toggle('easy_nav-toggle-show'));
+  const navs = document.querySelectorAll('.easy_nav');
+  for (var i = navs.length - 1; i >= 0; i--) {
+    navs[i].classList.toggle('easy_nav-toggle-show');
+  }
   document.getElementById('easy_nav-container').classList.toggle('easy_nav-mobile');
-  document.getElementsByClassName('easy_nav-toggle')[0].classList.toggle('easy_nav-toggle-show')
-  document.getElementById('easy_logo').classList.toggle('easy_logo-mobile')
+  document.getElementsByClassName('easy_nav-toggle')[0].classList.toggle('easy_nav-toggle-show');
+  document.getElementById('easy_logo').classList.toggle('easy_logo-mobile');
 }
 
 function navScroll() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById('easy_nav-container').classList.add('easy_nav-scroll')
-    document.getElementById('easy_logo').classList.add('easy_logo-scroll')
+    document.getElementById('easy_nav-container').classList.add('easy_nav-scroll');
+    document.getElementById('easy_logo').classList.add('easy_logo-scroll');
   } else {
-    document.getElementById('easy_nav-container').classList.remove('easy_nav-scroll')
-    document.getElementById('easy_logo').classList.remove('easy_logo-scroll')
+    document.getElementById('easy_nav-container').classList.remove('easy_nav-scroll');
+    document.getElementById('easy_logo').classList.remove('easy_logo-scroll');
   }
 }
 
