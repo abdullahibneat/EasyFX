d6c46d7460e12d1ff1a804d16765c57998c7e503
Abdullah Ibne Atiq <abdullahibneat@users.noreply.github.com>
2019-01-13 20:15:19 +0000

    Fix hamburger menu visibility, reset MailChimp cookie

--- js/custom.js
+++ js/custom.js
@@ -41,3 +41,5 @@ AOS.init({
 
 document.querySelector('.easy_nav-toggle').addEventListener('click', easy_nav_toggle);
 window.onscroll = function() {navScroll()};
+MCPopupClosed
+document.cookie = "MCPopupClosed=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
